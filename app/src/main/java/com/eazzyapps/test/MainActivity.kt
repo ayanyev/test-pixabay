37492b96b7c2c538d9752a14cd10c85043a5df45
Alex <alex@Alexs-MBP.fritz.box>
2022-01-15 13:09:20 +0100

    Add navigation

--- app/src/main/java/com/eazzyapps/test/MainActivity.kt
+++ app/src/main/java/com/eazzyapps/test/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
-import com.ezzyapps.test.repositories.ui.ThumbsListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,11 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, ThumbsListFragment.newInstance())
-                .commitNow()
-        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
