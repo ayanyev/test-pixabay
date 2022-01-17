b25a982547077b4cfb5c2979918e794ea2da6407
Alex <alex@Alexs-MBP.fritz.box>
2022-01-17 20:05:35 +0100

    Injected hit id via assisted inject

--- feature-repositories/src/main/java/com/ezzyapps/test/repositories/ui/imagedetails/ImageDetailsFragment.kt
+++ feature-repositories/src/main/java/com/ezzyapps/test/repositories/ui/imagedetails/ImageDetailsFragment.kt
@@ -1,30 +1,23 @@
 package com.ezzyapps.test.repositories.ui.imagedetails
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.ezzyapps.test.pixabay.common.BaseFragment
 import com.ezzyapps.test.repositories.R
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ImageDetailsFragment : BaseFragment<ImageDetailsViewModel>() {
 
-    override val layoutRes = R.layout.fragment_details
-    override val viewModel by viewModels<ImageDetailsViewModel>()
+    @Inject
+    lateinit var detailsViewModelFactory: ImageDetailsViewModel.AssistedFactory
 
     private val args by navArgs<ImageDetailsFragmentArgs>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val root = super.onCreateView(inflater, container, savedInstanceState)
-        viewModel.setId(args.imageId)
-        return root
+    override val layoutRes = R.layout.fragment_details
+    override val viewModel by viewModels<ImageDetailsViewModel> {
+        ImageDetailsViewModel.provideFactory(detailsViewModelFactory, args.imageId)
     }
 
 }
--- feature-repositories/src/main/java/com/ezzyapps/test/repositories/ui/imagedetails/ImageDetailsViewModel.kt
+++ feature-repositories/src/main/java/com/ezzyapps/test/repositories/ui/imagedetails/ImageDetailsViewModel.kt
@@ -2,28 +2,29 @@ package com.ezzyapps.test.repositories.ui.imagedetails
 
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.ezzyapps.test.pixabay.common.ActivityDelegate
 import com.ezzyapps.test.pixabay.common.BaseViewModel
 import com.ezzyapps.test.repositories.domain.ImageRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.subscribeBy
-import javax.inject.Inject
 
-@HiltViewModel
-class ImageDetailsViewModel @Inject constructor(
+class ImageDetailsViewModel @AssistedInject constructor(
 
+    @Assisted val hitId: Long,
     override val delegate: ActivityDelegate,
-    private val repo: ImageRepository
+    repo: ImageRepository
 
 ) : BaseViewModel() {
 
     val image = ObservableField<FullImageViewModel>()
 
-    fun setId(id: Long) {
+    init {
+
         disposables.add(
-            repo.getPhotoDetails(id)
+            repo.getPhotoDetails(hitId)
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnSubscribe {
                     delegate.showLoading(true)
@@ -43,6 +44,26 @@ class ImageDetailsViewModel @Inject constructor(
                     }
                 )
         )
+
+    }
+
+    @dagger.assisted.AssistedFactory
+    interface AssistedFactory {
+        fun create(hitId: Long): ImageDetailsViewModel
+    }
+
+    companion object {
+
+        fun provideFactory(
+            assistedFactory: AssistedFactory,
+            hitId: Long
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return assistedFactory.create(hitId) as T
+            }
+        }
+
     }
 
 }
